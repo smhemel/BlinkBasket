ce941cc4c5f4cc373ff70c1a2fb3017035b0b7f7
hemel <smhemel.eu@gmail.com>
2024-11-03 22:28:57 +0600

    Project setup - Backend.

---
+++ backend/server.js
@@ -0,0 +1,22 @@
+const cors = require("cors");
+const express = require("express");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const app = express();
+require("dotenv").config();
+const PORT = process.env.PORT || 5000;
+
+app.use(
+  cors({
+    origin: ["http://localhost:3000"],
+    credentials: true,
+  })
+);
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+app.use("/api", require("./routes/authRoutes"));
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
