8e2982029b4b97deb944b4b5ba6343bd844358d6
hemel <smhemel.eu@gmail.com>
2024-11-04 23:56:05 +0600

    Handle error and notification message - Dashboard

--- dashboard/src/store/Reducers/authReducer.js
+++ dashboard/src/store/Reducers/authReducer.js
@@ -3,11 +3,13 @@ import api from "../../api/api";
 
 export const admin_login = createAsyncThunk(
     'auth/admin_login',
-    async(info) => {
+    async(info, {rejectWithValue, fulfillWithValue}) => {
         try {
             const {data} = await api.post('/admin-login',info,{withCredentials: true});
+
+            return fulfillWithValue(data);
         } catch (error) {
-            
+            return rejectWithValue(error.response.data);
         }
     }
 )
@@ -20,8 +22,25 @@ export const authReducer = createSlice({
         loader: false,
         userInfo : ''
     },
-    reducers : { },
-    extraReducers: () => { }
+    reducers : {
+        messageClear: (state,_) => {
+            state.errorMessage = "";
+        }
+    },
+    extraReducers: (builder) => {
+        builder.addCase(admin_login.pending, (state, { payload }) => {
+            state.loader = true;
+        })
+        .addCase(admin_login.rejected, (state, { payload }) => {
+            state.loader = false;
+            state.errorMessage = payload.error;
+        })
+        .addCase(admin_login.fulfilled, (state, { payload }) => {
+            state.loader = false;
+            state.successMessage = payload.message
+        })
+    }
 });
 
+export const {messageClear} = authReducer.actions
 export default authReducer.reducer;
--- dashboard/src/views/auth/AdminLogin.jsx
+++ dashboard/src/views/auth/AdminLogin.jsx
@@ -1,10 +1,35 @@
-import React, { useState } from 'react';
-import { useDispatch } from 'react-redux';
-import { admin_login } from '../../store/Reducers/authReducer';
+import toast from 'react-hot-toast';
+import { useNavigate } from 'react-router-dom';
+import { PropagateLoader } from 'react-spinners';
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { admin_login, messageClear } from '../../store/Reducers/authReducer';
 
 const AdminLogin = () => {
+    const navigate = useNavigate();
     const dispatch = useDispatch();
+    const { loader, errorMessage, successMessage } = useSelector(state => state.auth);
     const [state, setState] = useState({ email: "", password: "" });
+
+    const overrideStyle = {
+        display : 'flex',
+        margin : '0 auto',
+        height: '22px',
+        justifyContent : 'center',
+        alignItem : 'center'
+    }
+
+    useEffect(() => {
+        if (errorMessage) {
+            toast.error(errorMessage);
+            dispatch(messageClear());
+        }
+        if (successMessage) {
+            toast.success(successMessage);
+            dispatch(messageClear());
+            navigate('/'); 
+        }
+    },[errorMessage, successMessage])
 
     const inputHandle = (e) => {
         setState({ ...state, [e.target.name]: e.target.value });
@@ -52,8 +77,10 @@ const AdminLogin = () => {
                             />
                         </div>
 
-                        <button className="bg-slate-800 w-full hover:shadow-blue-300/ hover:shadow-lg text-white rounded-md px-7 py-2 mb-3">
-                            Login
+                        <button disabled={loader ? true : false}  className='bg-slate-800 w-full hover:shadow-blue-300/ hover:shadow-lg text-white rounded-md px-7 py-2 mb-3'>
+                            {
+                                loader ? <PropagateLoader color='#fff' cssOverride={overrideStyle} /> : 'Login'
+                            }
                         </button>
                     </form>
                 </div>
