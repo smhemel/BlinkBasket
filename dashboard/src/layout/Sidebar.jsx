25705d914e7db6b49c3899d5b245423df1f51d25
hemel <smhemel.eu@gmail.com>
2024-11-05 21:02:39 +0600

    Design Header component and change some design of sidebar - Dashboard

--- dashboard/src/layout/Sidebar.jsx
+++ dashboard/src/layout/Sidebar.jsx
@@ -3,7 +3,7 @@ import React, { useEffect, useState } from 'react';
 import { Link, useLocation  } from 'react-router-dom';
 import { getNav } from '../navigation/index';
 
-const Sidebar = () => {
+const Sidebar = ({showSidebar, setShowSidebar}) => {
     const {pathname} = useLocation();
     const [allNav, setAllNav] = useState([]);
 
@@ -14,8 +14,8 @@ const Sidebar = () => {
 
     return (
         <div>
-           <div></div>
-            <div className={`w-[260px] fixed bg-[#e6e7fb] z-50 top-0 h-screen shadow-[0_0_15px_0_rgb(34_41_47_/_5%)] transition-all `}>
+           <div onClick={()=> setShowSidebar(false)} className={`fixed duration-200 ${!showSidebar ? 'invisible' : 'visible'} w-screen h-screen bg-[#8cbce780] top-0 left-0 z-10`}></div>
+            <div className={`w-[260px] fixed bg-[#e6e7fb] z-50 top-0 h-screen shadow-[0_0_15px_0_rgb(34_41_47_/_5%)] transition-all ${showSidebar ? 'left-0' : '-left-[260px] lg:left-0'}`}>
                 <div className='h-[70px] flex justify-center items-center'>
                     <Link to='/' className='w-[180px] h-[50px]'>
                         <img className='w-full h-full' src="http://localhost:3000/images/logo.png" alt="" />
