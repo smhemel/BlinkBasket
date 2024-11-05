749e0e78be77de973cd1ddc03be8f09b1ef51d0e
hemel <smhemel.eu@gmail.com>
2024-11-05 19:22:20 +0600

    Create Side Menu component and it's all elements - Dashboard

---
+++ dashboard/src/navigation/allNav.js
@@ -0,0 +1,65 @@
+import { MdPayment } from "react-icons/md";
+import { BiCategory } from "react-icons/bi";
+import { IoIosChatbubbles } from "react-icons/io";
+import { FaCodePullRequest } from "react-icons/fa6";
+import { FaUserTimes, FaUsers } from "react-icons/fa";
+import { AiOutlineDashboard, AiOutlineShoppingCart } from "react-icons/ai";
+
+export const allNav = [
+    {
+        id : 1,
+        title : 'Dashboard',
+        icon : <AiOutlineDashboard />,
+        role : 'admin',
+        path: '/admin/dashboard'
+    },
+    {
+        id : 2,
+        title : 'Orders',
+        icon : <AiOutlineShoppingCart />,
+        role : 'admin',
+        path: '/admin/dashboard/orders'
+    },
+    {
+        id : 3,
+        title : 'Category',
+        icon : <BiCategory  />,
+        role : 'admin',
+        path: '/admin/dashboard/category'
+    },
+    {
+        id : 4,
+        title : 'Sellers',
+        icon : <FaUsers   />,
+        role : 'admin',
+        path: '/admin/dashboard/sellers'
+    },
+    {
+        id : 5,
+        title : 'Payment Request',
+        icon : <MdPayment />,
+        role : 'admin',
+        path: '/admin/dashboard/payment-request'
+    },
+    {
+        id : 6,
+        title : 'Deactive Sellers',
+        icon : <FaUserTimes />,
+        role : 'admin',
+        path: '/admin/dashboard/deactive-sellers'
+    },
+    {
+        id : 7,
+        title : 'Seller Request',
+        icon : <FaCodePullRequest />,
+        role : 'admin',
+        path: '/admin/dashboard/sellers-request'
+    },
+    {
+        id : 8,
+        title : 'Live Chat',
+        icon : <IoIosChatbubbles />,
+        role : 'admin',
+        path: '/admin/dashboard/chat-seller'
+    }
+]
