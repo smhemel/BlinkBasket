4b37fd7cd1ef2cd91a8d98c898f2b89c5a3b9b7e
hemel <smhemel.eu@gmail.com>
2024-11-07 00:14:24 +0600

    Create Sellers component and design - Dashboard

--- dashboard/src/router/routes/adminRoutes.js
+++ dashboard/src/router/routes/adminRoutes.js
@@ -2,6 +2,7 @@ import { lazy } from "react";
 const AdminDashboard = lazy(()=> import('../../views/admin/AdminDashboard'));
 const Orders = lazy(()=> import('../../views/admin/Orders'));
 const Category = lazy(()=> import('../../views/admin/Category'));
+const Sellers = lazy(()=> import('../../views/admin/Sellers'));
 
 export const adminRoutes = [
     {
@@ -11,12 +12,17 @@ export const adminRoutes = [
     },
     {
         path: 'admin/dashboard/orders',
-        element : <Orders /> ,
+        element : <Orders />,
         role : 'admin'
     },
     {
         path: 'admin/dashboard/category',
-        element : <Category/> ,
+        element : <Category />,
+        role : 'admin'
+    },
+    {
+        path: 'admin/dashboard/sellers',
+        element : <Sellers />,
         role : 'admin'
     }
 ]
