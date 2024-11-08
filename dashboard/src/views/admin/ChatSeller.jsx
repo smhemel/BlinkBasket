850c998c8d86b1d44b55d2ceaf56ce0db82c0353
hemel <smhemel.eu@gmail.com>
2024-11-09 01:10:23 +0600

    Fix minor design - Dashboard

--- dashboard/src/views/admin/ChatSeller.jsx
+++ dashboard/src/views/admin/ChatSeller.jsx
@@ -42,22 +42,23 @@ const ChatSeller = () => {
                                     </div> 
                                 </div>
                             </div>
+                            
+                            <div className={`h-[60px] flex justify-start gap-2 items-center text-white px-2 py-2 rounded-sm cursor-pointer`}>
+                                <div className='relative'>
+                                    <img className='w-[38px] h-[38px] border-white border-2 max-w-[38px] p-[2px] rounded-full' src="http://localhost:3000/images/admin.jpg" alt="" />
+                                    <div className='w-[10px] h-[10px] bg-green-500 rounded-full absolute right-0 bottom-0'></div>
+                                </div>
+
+                                <div className='flex justify-center items-start flex-col w-full'>
+                                    <div className='flex justify-between items-center w-full'>
+                                        <h2 className='text-base font-semibold'>Raju</h2>
+                                    </div> 
+                                </div> 
+                            </div>
                         </div> 
                     </div>
 
 
-                    <div className={`h-[60px] flex justify-start gap-2 items-center text-white px-2 py-2 rounded-sm cursor-pointer`}>
-                        <div className='relative'>
-                            <img className='w-[38px] h-[38px] border-white border-2 max-w-[38px] p-[2px] rounded-full' src="http://localhost:3000/images/admin.jpg" alt="" />
-                            <div className='w-[10px] h-[10px] bg-green-500 rounded-full absolute right-0 bottom-0'></div>
-                        </div>
-
-                        <div className='flex justify-center items-start flex-col w-full'>
-                            <div className='flex justify-between items-center w-full'>
-                                <h2 className='text-base font-semibold'>Raju</h2>
-                            </div> 
-                        </div> 
-                    </div>
 
                     <div className='w-full md:w-[calc(100%-200px)] md:pl-4'>
                         <div className='flex justify-between items-center'>
