40523708729144cd58754974aa77762b70c4a7f3
hemel <smhemel.eu@gmail.com>
2024-11-09 01:41:03 +0600

    Add Nav items for seller and Routes - Dashbaord

--- dashboard/src/router/routes/sellerRoutes.js
+++ dashboard/src/router/routes/sellerRoutes.js
@@ -1,10 +1,22 @@
 import { lazy } from "react";
 const Home = lazy(()=> import('../../views/Home'));
+const AddProduct = lazy(()=> import('../../views/seller/AddProduct'));
+const SellerDashboard = lazy(()=> import('../../views/seller/SellerDashboard'));
 
 export const sellerRoutes = [
     {
         path: '/',
         element: <Home />,
         ability: ['admin','seller']
+    },
+    {
+        path: '/seller/dashboard',
+        element : <SellerDashboard />,
+        ability : ['seller']
+    },
+    {
+        path: '/seller/dashboard/add-product',
+        element : <AddProduct />,
+        ability : ['seller']
     }
 ]
