91f122bb1d81afe97468ce6b8a91d42a339f79e1
hemel <smhemel.eu@gmail.com>
2024-11-09 01:43:01 +0600

    Create Add Product component for seller - Dashboard

---
+++ dashboard/src/views/seller/AddProduct.jsx
@@ -0,0 +1,142 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+
+const AddProduct = () => {
+    const categorys = [
+        { id: 1, name : 'Sports' },
+        { id: 2, name : 'Tshirt' },
+        { id: 3, name : 'Mobile' },
+        { id: 4, name : 'Computer' },
+        { id: 5, name : 'Watch' },
+        { id: 6, name : 'Pant' }
+    ];
+
+    const [state, setState] = useState({
+        name: "",
+        description: "",
+        discount: "",
+        price: "",
+        brand: "",
+        stock: "",
+    });
+    const [category, setCategory] = useState('');
+    const [cateShow, setCateShow] = useState(false);
+    const [allCategory, setAllCategory] = useState(categorys);
+    const [searchValue, setSearchValue] = useState('');
+
+    const categorySearch = (e) => {
+        const value = e.target.value;
+        setSearchValue(value);
+
+        if (value) {
+            let srcValue = allCategory.filter(c => c.name.toLowerCase().indexOf(value.toLowerCase()) > -1);
+            setAllCategory(srcValue);
+        } else {
+            setAllCategory(categorys);
+        }
+    };
+
+    const inputHandle = (e) => {
+        setState({
+        ...state,
+        [e.target.name]: e.target.value,
+        });
+    };
+
+    return (
+        <div className="px-2 lg:px-7 pt-5">
+            <div className="w-full p-4 bg-[#6a5fdf] rounded-md">
+                <div className="flex justify-between items-center pb-4">
+                    <h1 className="text-[#d0d2d6] text-xl font-semibold">Add Product</h1>
+                    <Link className="bg-blue-500 hover:shadow-blue-500/50 hover:shadow-lg text-white rounded-sm px-7 py-2 my-2">
+                        All Product
+                    </Link>
+                </div>
+                <div>
+                    <form>
+                        <div className="flex flex-col mb-3 md:flex-row gap-4 w-full text-[#d0d2d6]">
+                            <div className="flex flex-col w-full gap-1">
+                                <label htmlFor="name">Product Name</label>
+                                <input
+                                    className="px-4 py-2 focus:border-indigo-500 outline-none bg-[#6a5fdf] border border-slate-700 rounded-md text-[#d0d2d6]"
+                                    onChange={inputHandle}
+                                    value={state.name}
+                                    type="text"
+                                    name="name"
+                                    id="name"
+                                    placeholder="Product Name"
+                                />
+                            </div>
+                            <div className='flex flex-col w-full gap-1'>
+                                <label htmlFor="brand">Product Brand</label>
+                                <input className='px-4 py-2 focus:border-indigo-500 outline-none bg-[#6a5fdf] border border-slate-700 rounded-md text-[#d0d2d6]' onChange={inputHandle} value={state.brand} type="text" name='brand' id='brand' placeholder='Brand Name' />
+                            </div> 
+                        </div>
+
+                        <div className='flex flex-col mb-3 md:flex-row gap-4 w-full text-[#d0d2d6]'>
+                            <div className='flex flex-col w-full gap-1 relative'>
+                                <label htmlFor="category">Category</label>
+                                <input 
+                                    readOnly 
+                                    className='px-4 py-2 focus:border-indigo-500 outline-none bg-[#6a5fdf] border border-slate-700 rounded-md text-[#d0d2d6]' 
+                                    id='category'
+                                    type="text" 
+                                    placeholder='--select category--'
+                                    onClick={()=> setCateShow(!cateShow)} 
+                                    onChange={inputHandle} 
+                                    value={category} 
+                                />
+                                <div className={`absolute top-[101%] bg-slate-800 w-full transition-all ${cateShow ? 'scale-100' : 'scale-0'}`}>
+                                    <div className='w-full px-4 py-2 fixed'>
+                                    <input 
+                                        className='px-3 py-1 w-full focus:border-indigo-500 outline-none bg-transparent border border-slate-700 rounded-md text-[#d0d2d6] overflow-hidden' 
+                                        type="text" 
+                                        placeholder='search'
+                                        value={searchValue}
+                                        onChange={categorySearch} 
+                                    /> 
+                                    </div>
+                                    <div className='pt-14'></div>
+                                    <div className='flex justify-start items-start flex-col h-[200px] overflow-x-scroll'>
+                                        {
+                                            allCategory.map((c,i) => <span
+                                                className={`px-4 py-2 hover:bg-indigo-500 hover:text-white hover:shadow-lg w-full cursor-pointer ${category === c.name && 'bg-indigo-500'}`}
+                                                key={i}
+                                                onClick={()=> {
+                                                    setCateShow(false)
+                                                    setCategory(c.name)
+                                                    setSearchValue('')
+                                                    setAllCategory(categorys)
+                                            }}>{c.name} </span> )
+                                        } 
+                                    </div>
+                                </div>
+                            </div>  
+                            <div className='flex flex-col w-full gap-1'>
+                                <label htmlFor="stock">Product Stock</label>
+                                <input className='px-4 py-2 focus:border-indigo-500 outline-none bg-[#6a5fdf] border border-slate-700 rounded-md text-[#d0d2d6]' onChange={inputHandle} value={state.stock} type="text" name='stock' id='stock' placeholder='Stock' />
+                            </div>   
+                        </div>
+
+                        <div className='flex flex-col mb-3 md:flex-row gap-4 w-full text-[#d0d2d6]'>
+                            <div className='flex flex-col w-full gap-1'>
+                                <label htmlFor="price">Price</label>
+                                <input className='px-4 py-2 focus:border-indigo-500 outline-none bg-[#6a5fdf] border border-slate-700 rounded-md text-[#d0d2d6]' onChange={inputHandle} value={state.price} type="number" name='price' id='price' placeholder='price' />
+                            </div>  
+                            <div className='flex flex-col w-full gap-1'>
+                                <label htmlFor="discount">Discount</label>
+                                <input className='px-4 py-2 focus:border-indigo-500 outline-none bg-[#6a5fdf] border border-slate-700 rounded-md text-[#d0d2d6]' onChange={inputHandle} value={state.discount} type="number" name='discount' id='discount' placeholder='discount by %' />
+                            </div>   
+                        </div>
+                        <div className='flex flex-col w-full gap-1'>
+                            <label htmlFor="description" className='text-[#d0d2d6]'>Description</label>
+                            <textarea className='px-4 py-2 focus:border-indigo-500 outline-none bg-[#6a5fdf] border border-slate-700 rounded-md text-[#d0d2d6]' onChange={inputHandle} value={state.description} name='description' id='description' placeholder='Description' cols="10" rows="4"></textarea> 
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default AddProduct;
