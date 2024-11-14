7ac0005a1d5715b4c9f02d6d71ea933ff036dd70
hemel <smhemel.eu@gmail.com>
2024-11-14 15:41:13 +0600

    Implement category delete functionality - Backend

--- backend/controllers/dashboard/categoryController.js
+++ backend/controllers/dashboard/categoryController.js
@@ -105,6 +105,21 @@ class categoryController {
             }     
         })
     }
+
+    deleteCategory = async (req, res) => {
+        try {
+            const categoryId = req.params.id;
+            const deleteCategory = await categoryModel.findByIdAndDelete(categoryId);
+
+            if (!deleteCategory) {
+                return res.status(404).json({message: 'Category not found'});
+            }
+
+            res.status(200).json({message: 'Category deleted successfully'});
+        } catch (error) {
+            res.status(500).json({message: 'Internal Server Error'});
+        }
+    }
 }
 
 module.exports = new categoryController();
