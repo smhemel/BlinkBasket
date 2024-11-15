d6e6a4c8d5183e0aecd3a47722e9fc1aa06a9094
hemel <smhemel.eu@gmail.com>
2024-11-16 00:18:30 +0600

    Initial project setup - Frontend

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Header from '../components/Header';
+
+const Home = () => {
+    return (
+        <div className='w-full'>
+        </div>
+    );
+};
+
+export default Home;
