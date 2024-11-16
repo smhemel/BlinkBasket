cf8d4145fa02e7efca52580d080e805268d40faf
hemel <smhemel.eu@gmail.com>
2024-11-16 11:46:00 +0600

    Create and design products components - Frontend

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from '../components/Header';
 import Banner from '../components/Banner';
 import Categories from '../components/Categories';
+import Products from '../components/products/Products';
 import FeatureProducts from '../components/products/FeatureProducts';
 
 const Home = () => {
@@ -12,6 +13,22 @@ const Home = () => {
             <Categories />
             <div className='py-[45px]'>
                 <FeatureProducts/>
+            </div>
+
+            <div className='py-10'>
+                <div className='w-[85%] flex flex-wrap mx-auto'>
+                    <div className='grid w-full grid-cols-3 md-lg:grid-cols-2 md:grid-cols-1 gap-7'>
+                        <div className='overflow-hidden'>
+                            <Products title='Latest Product' />
+                        </div>
+                        <div className='overflow-hidden'>
+                            <Products title='Top Rated Product' />
+                        </div>
+                        <div className='overflow-hidden'>
+                            <Products title='Discount Product'/>
+                        </div>
+                    </div> 
+                </div> 
             </div>
         </div>
     );
