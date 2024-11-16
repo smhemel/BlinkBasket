c86a8ed1f01e49c007b9b6ee8b26e2bc07dac259
hemel <smhemel.eu@gmail.com>
2024-11-16 10:57:34 +0600

    Create and design product feature component - Frontend

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from '../components/Header';
 import Banner from '../components/Banner';
 import Categories from '../components/Categories';
+import FeatureProducts from '../components/products/FeatureProducts';
 
 const Home = () => {
     return (
@@ -9,6 +10,9 @@ const Home = () => {
             <Header />
             <Banner />
             <Categories />
+            <div className='py-[45px]'>
+                <FeatureProducts/>
+            </div>
         </div>
     );
 };
