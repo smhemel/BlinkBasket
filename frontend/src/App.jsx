16457ca5908f97be739dc707a905cb415f215f8c
hemel <smhemel.eu@gmail.com>
2024-11-18 20:25:25 +0600

    Create and design Card page - Frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import Card from './pages/Card';
 import Home from "./pages/Home";
 import Shops from './pages/Shops';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
@@ -8,7 +9,8 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path='/shops' element={<Shops/>} />
+        <Route path='/shops' element={<Shops />} />
+        <Route path='/card' element={<Card />} />
       </Routes>
     </BrowserRouter>
   );
