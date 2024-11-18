ae2d87c6b513305b2ce28b5fd5685a401fa7d197
hemel <smhemel.eu@gmail.com>
2024-11-18 22:51:01 +0600

    Create and design Shipping page - Frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import "./App.css";
 import Card from './pages/Card';
 import Home from "./pages/Home";
 import Shops from './pages/Shops';
+import Shipping from './pages/Shipping';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path='/shops' element={<Shops />} />
         <Route path='/card' element={<Card />} />
+        <Route path='/shipping' element={<Shipping />} />
       </Routes>
     </BrowserRouter>
   );
