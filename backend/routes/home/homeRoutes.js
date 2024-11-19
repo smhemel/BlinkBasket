81dabe1daa4b6cc5504caaf5e7855523a8580fa3
hemel <smhemel.eu@gmail.com>
2024-11-20 01:28:15 +0600

    create get products API to retrieve all products - Backend

--- backend/routes/home/homeRoutes.js
+++ backend/routes/home/homeRoutes.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const homeControllers = require('../../controllers/home/homeControllers');
 
+router.get('/get-products', homeControllers.get_products);
 router.get('/get-categories', homeControllers.get_categories);
  
 module.exports = router;
