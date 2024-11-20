6c582f1d26019d04f5c4ba0cecf8bf256874100f
hemel <smhemel.eu@gmail.com>
2024-11-20 11:53:50 +0600

    Create API for query products based on query - Backend

--- backend/controllers/home/homeControllers.js
+++ backend/controllers/home/homeControllers.js
@@ -1,6 +1,7 @@
 const productModel = require('../../models/productModel');
 const categoryModel = require('../../models/categoryModel');
 const { responseReturn } = require("../../utiles/response");
+const queryProducts = require('../../utiles/queryProducts');
 
 class homeControllers {
     formatProduct = (products) => {
@@ -73,6 +74,22 @@ class homeControllers {
                 priceRange
             });
         } catch (error) { }
+    }
+
+    query_products = async (req, res) => {
+        const parPage = 12;
+        req.query.parPage = parPage;
+
+        try {
+            const products = await productModel.find({}).sort({ createdAt: -1 });
+
+            const totalProduct = new queryProducts(products, req.query).categoryQuery().ratingQuery().priceQuery().sortByPrice().countProducts();
+            const result = new queryProducts(products, req.query).categoryQuery().ratingQuery().priceQuery().sortByPrice().skip().limit().getProducts();
+            
+            responseReturn(res, 200, { products: result, totalProduct, parPage });
+        } catch (error) {
+            
+        }
     }
 }
 
