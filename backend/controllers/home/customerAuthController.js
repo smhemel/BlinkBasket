862a671bee6b24c4ef9a5d29df8fb7cf1dbbb66c
hemel <smhemel.eu@gmail.com>
2024-11-20 17:22:20 +0600

    Implement customer registration functionality - Backend

---
+++ backend/controllers/home/customerAuthController.js
@@ -0,0 +1,45 @@
+const bcrypt = require('bcrypt');
+const {createToken} = require('../../utiles/tokenCreate')
+const customerModel = require('../../models/customerModel');
+const { responseReturn } = require('../../utiles/response');
+const sellerCustomerModel = require('../../models/chat/sellerCustomerModel');
+
+class customerAuthController {
+    customer_register = async(req, res) => {
+        const {name, email, password } = req.body;
+
+        try {
+            const customer = await customerModel.findOne({email});
+
+            if (customer) {
+                responseReturn(res, 404, {error: 'Email Already Exits'});
+            } else {
+                const createCustomer = await customerModel.create({
+                    name: name.trim(),
+                    email: email.trim(),
+                    password: await bcrypt.hash(password, 10),
+                    method: 'menualy'
+                });
+
+                await sellerCustomerModel.create({myId: createCustomer.id});
+
+                const token = await createToken({
+                    id: createCustomer.id,
+                    name: createCustomer.name,
+                    email: createCustomer.email,
+                    method: createCustomer.method
+                });
+
+                res.cookie('customerToken', token, {
+                    expires: new Date(Date.now() + 7*24*60*60*1000 )
+                });
+
+                responseReturn(res, 201, {message: "User Register Success", token});
+            }
+        } catch (error) {
+            
+        }
+    }
+}
+
+module.exports = new customerAuthController();
