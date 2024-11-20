ed6feb934ea3aba740a0ca2a905777c7924ac0e7
hemel <smhemel.eu@gmail.com>
2024-11-21 00:28:56 +0600

    Create get_card_products method with all the calculation - Backend

--- backend/routes/home/cardRoutes.js
+++ backend/routes/home/cardRoutes.js
@@ -2,5 +2,6 @@ const router = require('express').Router();
 const cardController = require('../../controllers/home/cardController');
 
 router.post('/home/product/add-to-card', cardController.add_to_card);
+router.get('/home/product/get-card-product/:userId', cardController.get_card_products);
 
 module.exports = router;
