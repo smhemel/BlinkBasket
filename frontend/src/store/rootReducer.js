99f828384b54cd725d4cb2dbf3227b78621dfc1c
hemel <smhemel.eu@gmail.com>
2024-11-20 22:48:58 +0600

    Implement product add to card functionality - Frontend

--- frontend/src/store/rootReducer.js
+++ frontend/src/store/rootReducer.js
@@ -1,9 +1,11 @@
 import authReducer from "./reducers/authReducer";
 import homeReducer from "./reducers/homeReducer";
+import cardReducer from "./reducers/cardReducer";
 
 const rootReducer = {
     home: homeReducer,
-    auth: authReducer
+    auth: authReducer,
+    card: cardReducer
 }
 
 export default rootReducer;
