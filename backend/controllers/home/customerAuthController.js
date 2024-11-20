ee3dff59f1989db04001236c39e469bdf2c43e90
hemel <smhemel.eu@gmail.com>
2024-11-20 21:45:14 +0600

    Implement login API - Backend

--- backend/controllers/home/customerAuthController.js
+++ backend/controllers/home/customerAuthController.js
@@ -42,4 +42,38 @@ class customerAuthController {
     }
 }
 
+customer_login = async(req, res) => {
+    const { email, password } = req.body;
+       
+    try {
+        const customer = await customerModel.findOne({email}).select('+password');
+
+        if (customer) {
+            const match = await bcrypt.compare(password, customer.password);
+            
+            if (match) {
+                const token = await createToken({
+                    id : customer.id,
+                    name: customer.name,
+                    email: customer.email,
+                    method: customer.method 
+                });
+
+                res.cookie('customerToken', token, {
+                    expires : new Date(Date.now() + 7*24*60*60*1000 )
+                });
+
+                responseReturn(res, 201, {message:  'User Login Success', token});
+            } else {
+                responseReturn(res, 404, {error:  'Password Wrong'});
+            }
+        } else {
+            responseReturn(res, 404, {error:  'Email Not Found'});
+        }
+        
+    } catch (error) {
+
+    }
+}
+
 module.exports = new customerAuthController();
--- backend/routes/home/customerAuthRoutes.js
+++ backend/routes/home/customerAuthRoutes.js
@@ -2,5 +2,6 @@ const router = require('express').Router();
 const customerAuthController = require('../../controllers/home/customerAuthController');
 
 router.post('/customer/customer-register', customerAuthController.customer_register);
+router.post('/customer/customer-login', customerAuthController.customer_login);
  
 module.exports = router;
