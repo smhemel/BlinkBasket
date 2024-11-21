8346794e77944f5ba952233c741119a675b4b114
hemel <smhemel.eu@gmail.com>
2024-11-21 12:32:55 +0600

    Implement functionality to display customer dashboard data - Frontend

--- frontend/src/components/dashboard/Index.jsx
+++ frontend/src/components/dashboard/Index.jsx
@@ -1,8 +1,34 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 import { RiShoppingCart2Fill } from "react-icons/ri";
+import { useDispatch, useSelector } from 'react-redux';
+import { get_dashboard_index_data } from '../../store/reducers/dashboardReducer';
 
 const Index = () => {
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
+    const {userInfo} = useSelector(state => state.auth);
+    const {recentOrders, totalOrder, pendingOrder, cancelledOrder} = useSelector(state => state.dashboard);
+
+    useEffect(() => {
+        dispatch(get_dashboard_index_data(userInfo.id));
+    },[])
+
+    const redirect = (ord) => {
+        let items = 0;
+        for (let i = 0; i < ord.length; i++) {
+            items = ord.products[i].quantity + items; 
+        }
+
+        navigate('/payment', {
+            state: {
+                price: ord.price,
+                items,
+                orderId: ord._id 
+            }
+        });
+    };
+
     return (
         <div>
             <div className='grid grid-cols-3 md:grid-cols-1 gap-5'>
@@ -11,7 +37,7 @@ const Index = () => {
                         <span className='text-xl text-green-800'><RiShoppingCart2Fill /></span>
                     </div>
                     <div className='flex flex-col justify-start items-start text-slate-600'>
-                        <h2 className='text-3xl font-bold'>45</h2>
+                        <h2 className='text-3xl font-bold'>{totalOrder}</h2>
                         <span>Orders </span>
                     </div>     
                 </div>
@@ -20,7 +46,7 @@ const Index = () => {
                         <span className='text-xl text-green-800'><RiShoppingCart2Fill /></span>
                     </div>
                     <div className='flex flex-col justify-start items-start text-slate-600'>
-                        <h2 className='text-3xl font-bold'>25</h2>
+                        <h2 className='text-3xl font-bold'>{pendingOrder}</h2>
                         <span>Pending Orders </span>
                     </div>     
                 </div>
@@ -29,11 +55,12 @@ const Index = () => {
                         <span className='text-xl text-green-800'><RiShoppingCart2Fill /></span>
                     </div>
                     <div className='flex flex-col justify-start items-start text-slate-600'>
-                        <h2 className='text-3xl font-bold'>2</h2>
-                        <span>Cancelled Orders </span>
+                        <h2 className='text-3xl font-bold'>{cancelledOrder}</h2>
+                        <span>Cancelled Orders</span>
                     </div>     
                 </div> 
             </div>
+
             <div className='bg-white p-5 mt-5 rounded-md'>
                 <h2>Recent Orders</h2>
                 <div className='pt-4'>
@@ -49,26 +76,22 @@ const Index = () => {
                                 </tr>
                             </thead>
                             <tbody>
-                                <tr className='bg-white border-b'>
-                                    <td scope='row' className='px-6 py-4 font-medium whitespace-nowrap'>#344</td>
-                                    <td scope='row' className='px-6 py-4 font-medium whitespace-nowrap'>$233</td>
-                                    <td scope='row' className='px-6 py-4 font-medium whitespace-nowrap'>pending</td>
-                                    <td scope='row' className='px-6 py-4 font-medium whitespace-nowrap'>pending</td>
-                                    <td scope='row' className='px-6 py-4 font-medium whitespace-nowrap'>
-                                        <Link><span className='bg-green-200 text-green-800 text-md font-semibold mr-2 px-3 py-[2px] rounded'>View</span></Link>
-                                        <Link><span className='bg-green-200 text-green-800 text-md font-semibold mr-2 px-3 py-[2px] rounded'>Pay Now</span></Link> 
-                                    </td> 
-                                </tr>
-                                <tr className='bg-white border-b'>
-                                    <td scope='row' className='px-6 py-4 font-medium whitespace-nowrap'>#344</td>
-                                    <td scope='row' className='px-6 py-4 font-medium whitespace-nowrap'>$233</td>
-                                    <td scope='row' className='px-6 py-4 font-medium whitespace-nowrap'>pending</td>
-                                    <td scope='row' className='px-6 py-4 font-medium whitespace-nowrap'>pending</td>
-                                    <td scope='row' className='px-6 py-4 font-medium whitespace-nowrap'>
-                                        <Link><span className='bg-green-200 text-green-800 text-md font-semibold mr-2 px-3 py-[2px] rounded'>View</span></Link>
-                                        <Link><span className='bg-green-200 text-green-800 text-md font-semibold mr-2 px-3 py-[2px] rounded'>Pay Now</span></Link> 
-                                    </td> 
-                                </tr>
+                                { recentOrders.map((o,i) => 
+                                    <tr className='bg-white border-b'>
+                                        <td scope='row' className='px-6 py-4 font-medium whitespace-nowrap'>#{o._id}</td>
+                                        <td scope='row' className='px-6 py-4 font-medium whitespace-nowrap'>${o.price}</td>
+                                        <td scope='row' className='px-6 py-4 font-medium whitespace-nowrap'>{o.payment_status }</td>
+                                        <td scope='row' className='px-6 py-4 font-medium whitespace-nowrap'>{o.delivery_status}</td>
+                                        <td scope='row' className='px-6 py-4 font-medium whitespace-nowrap'>
+                                            <Link to={`/dashboard/order/details/${o._id}`}><span className='bg-green-200 text-green-800 text-md font-semibold mr-2 px-3 py-[2px] rounded'>View</span></Link>
+                                            { 
+                                                o.payment_status !== 'paid' && 
+                                                <span onClick={() => redirect(o)} className='bg-green-200 text-green-800 text-md font-semibold mr-2 px-3 py-[2px] rounded cursor-pointer'>Pay Now</span> 
+                                            }
+                                        </td> 
+                                    </tr>
+                                    )
+                                }
                             </tbody>
                         </table>
                     </div>
--- frontend/src/store/rootReducer.js
+++ frontend/src/store/rootReducer.js
@@ -2,12 +2,14 @@ import authReducer from "./reducers/authReducer";
 import homeReducer from "./reducers/homeReducer";
 import cardReducer from "./reducers/cardReducer";
 import orderReducer from "./reducers/orderReducer";
+import dashboardReducer from "./reducers/dashboardReducer";
 
 const rootReducer = {
     home: homeReducer,
     auth: authReducer,
     card: cardReducer,
-    order: orderReducer
+    order: orderReducer,
+    dashboard: dashboardReducer
 }
 
 export default rootReducer;
