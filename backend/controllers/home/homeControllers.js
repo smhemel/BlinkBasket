b9a28a8f650bd36805a3b58bb90e9c7cfdb75653
hemel <smhemel.eu@gmail.com>
2024-11-22 23:08:58 +0600

    Implement submit review API - Backend

--- backend/controllers/home/homeControllers.js
+++ backend/controllers/home/homeControllers.js
@@ -1,7 +1,9 @@
+const moment = require('moment');
+const reviewModel = require('../../models/reviewModel');
 const productModel = require('../../models/productModel');
-const categoryModel = require('../../models/categoryModel');
 const { responseReturn } = require("../../utils/response");
 const queryProducts = require('../../utils/queryProducts');
+const categoryModel = require('../../models/categoryModel');
 
 class homeControllers {
     formatProduct = (products) => {
@@ -117,6 +119,36 @@ class homeControllers {
                 relatedProducts,
                 moreProducts
             });
+        } catch (error) {
+            console.log(error.message)
+        }
+    }
+
+    submit_review = async (req, res) => {
+        const {productId, rating, review, name} = req.body;
+
+        try {
+            await reviewModel.create({
+                productId,
+                name,
+                rating,
+                review,
+                date: moment(Date.now()).format('LL')
+            });
+
+            let rat = 0;
+            const reviews = await reviewModel.find({ productId });
+            for (let i = 0; i < reviews.length; i++) {
+                rat = rat + reviews[i].rating;
+            }
+
+            let productRating = 0;
+            if (reviews.length !== 0) {
+                productRating = (rat / reviews.length).toFixed(1);
+            }
+            await productModel.findByIdAndUpdate(productId, { rating : productRating });
+
+            responseReturn(res, 201, { message: "Review Added Successfully" });
         } catch (error) {
             console.log(error.message)
         }
--- backend/routes/home/homeRoutes.js
+++ backend/routes/home/homeRoutes.js
@@ -6,5 +6,6 @@ router.get('/get-categories', homeControllers.get_categories);
 router.get('/price-range-latest-product', homeControllers.price_range_product);
 router.get('/query-products', homeControllers.query_products);
 router.get('/product-details/:slug', homeControllers.product_details);
+router.post('/customer/submit-review', homeControllers.submit_review);
  
 module.exports = router;
