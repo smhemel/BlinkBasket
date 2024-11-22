e5d7dc60ab7a1aa122ae2f3c6c46c4fffc63d1da
hemel <smhemel.eu@gmail.com>
2024-11-22 22:45:45 +0600

    Add product to wishlist from product details page - Frontend

--- frontend/src/pages/Details.jsx
+++ frontend/src/pages/Details.jsx
@@ -10,13 +10,13 @@ import Carousel from 'react-multi-carousel';
 import 'react-multi-carousel/lib/styles.css';
 import {Swiper, SwiperSlide } from 'swiper/react';
 import React, { useEffect, useState } from 'react';
-import { Link, useNavigate, useParams } from 'react-router-dom';
 import { IoIosArrowForward } from "react-icons/io";
 import { FaHeart, FaTwitter } from "react-icons/fa6";
 import { useDispatch, useSelector } from 'react-redux';
 import { product_details } from '../store/reducers/homeReducer';
+import { Link, useNavigate, useParams } from 'react-router-dom';
 import { FaFacebookF, FaLinkedin, FaGithub } from "react-icons/fa";
-import { add_to_card, messageClear } from '../store/reducers/cardReducer';
+import { add_to_card, messageClear, add_to_wishlist } from '../store/reducers/cardReducer';
 
 const Details = () => {
     const {slug} = useParams();
@@ -101,6 +101,23 @@ const Details = () => {
         }
     }
 
+    const add_wishlist = () => {
+        if (userInfo) {
+            dispatch(add_to_wishlist({
+                userId: userInfo.id,
+                productId: product._id,
+                name: product.name,
+                price: product.price,
+                image: product.images[0],
+                discount: product.discount,
+                rating: product.rating,
+                slug: product.slug
+            }));
+        } else {
+            navigate('/login');
+        }
+    }
+
     return (
         <div>
             <Header/>
@@ -133,7 +150,7 @@ const Details = () => {
                 </div>
             </section>
             <section>
-                <div className='w-[85%] md:w-[80%] sm:w-[90%] lg:w-[90%] h-full mx-auto'>
+                <div className='w-[85%] md:w-[80%] sm:w-[90%] lg:w-[90%] h-full mx-auto pb-16'>
                     <div className='grid grid-cols-2 md-lg:grid-cols-1 gap-8'>
                         <div>
                             <div className='p-5 border'>
@@ -192,7 +209,7 @@ const Details = () => {
                                     </> : ''
                                 }
                                 <div>
-                                    <div className='h-[50px] w-[50px] flex justify-center items-center cursor-pointer hover:shadow-lg hover:shadow-cyan-500/40 bg-cyan-500 text-white'>
+                                    <div onClick={add_wishlist} className='h-[50px] w-[50px] flex justify-center items-center cursor-pointer hover:shadow-lg hover:shadow-cyan-500/40 bg-cyan-500 text-white'>
                                         <FaHeart />
                                     </div>
                                 </div>
