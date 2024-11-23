6ab41a7d18d23da3085aedc279e5f3d37ed454f9
hemel <smhemel.eu@gmail.com>
2024-11-23 20:44:27 +0600

    Implement API for customer to seller message add - Backend

--- backend/routes/chat/chatRoutes.js
+++ backend/routes/chat/chatRoutes.js
@@ -3,5 +3,6 @@ const { authMiddleware } = require('../middlewares/authMiddleware');
 const chatController = require('../controllers/chat/chatController');
 
 router.post('/chat/customer/add-customer-friend', chatController.add_customer_friend);
+router.post('/chat/customer/send-message-to-seller', chatController.customer_message_add);
  
 module.exports = router;
