6e0efb1f5e023c34681b228fc0379b64a543dbd7
hemel <smhemel.eu@gmail.com>
2024-11-27 00:43:45 +0600

    Implement API to get deactive sellers - Backend

--- backend/routes/dashboard/sellerRoutes.js
+++ backend/routes/dashboard/sellerRoutes.js
@@ -6,5 +6,6 @@ router.get('/request-seller-get', authMiddleware, sellerController.request_selle
 router.get('/get-seller/:sellerId', authMiddleware, sellerController.get_seller);
 router.post('/seller-status-update', authMiddleware, sellerController.seller_status_update);
 router.get('/get-sellers', authMiddleware, sellerController.get_active_sellers);
+router.get('/get-deactive-sellers',authMiddleware, sellerController.get_deactive_sellers);
 
 module.exports = router;
