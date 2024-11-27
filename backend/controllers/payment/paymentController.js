8910aa0562b2db4969ad4be608c9d6387dc93f1d
hemel <smhemel.eu@gmail.com>
2024-11-27 23:06:45 +0600

    Implement API to active stripe account - Backend

--- backend/controllers/payment/paymentController.js
+++ backend/controllers/payment/paymentController.js
@@ -1,4 +1,5 @@
 const {v4: uuidv4} = require('uuid');
+const sellerModel = require('../../models/sellerModel');
 const stripeModel = require('../../models/stripeModel');
 const { responseReturn } = require('../../utiles/response');
 const stripe = require('stripe')('sk_test_51QPlfMGSOVK8WpbF8kcliA8uNU1iylSVy0d6JbEJYsXXcvzuZ6Ljf17n2YuKJaeqR98Px8VhXui0aW9U5NX5OaoY00IsbkFyfX');
@@ -43,6 +44,24 @@ class paymentController{
             console.log('strpe connect account errror' + error.message);
         }
     }
+
+    active_stripe_connect_account = async (req, res) => {
+        const {id} = req;
+        const {activeCode} = req.params;
+
+        try {
+            const userStripeInfo = await stripeModel.findOne({ code: activeCode });
+            
+            if (userStripeInfo) {
+                await sellerModel.findByIdAndUpdate(id, { payment: 'active' });
+                responseReturn(res, 200, {message: 'payment Active'});
+            } else {
+                responseReturn(res, 404, {message: 'payment Active Fails'});
+            } 
+        } catch (error) {
+            responseReturn(res, 500, {message: 'Internal Server Error'});
+        } 
+     }
 }
 
 module.exports = new paymentController();
--- backend/routes/paymentRoutes.js
+++ backend/routes/paymentRoutes.js
@@ -3,5 +3,6 @@ const { authMiddleware } = require('../middlewares/authMiddleware');
 const paymentController = require('../controllers/payment/paymentController');
  
 router.get('/payment/create-stripe-connect-account', authMiddleware, paymentController.create_stripe_connect_account);
+router.put('/payment/active-stripe-connect-account/:activeCode', authMiddleware, paymentController.active_stripe_connect_account)
  
 module.exports = router;
