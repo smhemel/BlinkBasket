b07dc71ad20874eae0f588c733d26104ebd031d4
hemel <smhemel.eu@gmail.com>
2024-11-28 16:14:24 +0600

    Implement API to get seller data for dashboard - Backend

--- backend/controllers/dashboard/dashboardController.js
+++ backend/controllers/dashboard/dashboardController.js
@@ -1,9 +1,13 @@
-const { responseReturn } = require("../../utiles/response");
+const { mongo: {ObjectId}} = require('mongoose');
+const authOrder = require('../../models/authOrder'); 
+const sellerModel = require('../../models/sellerModel');
 const myShopWallet = require('../../models/myShopWallet');
 const productModel = require('../../models/productModel');
+const sellerWallet = require('../../models/sellerWallet'); 
 const customerOrder = require('../../models/customerOrder');
-const sellerModel = require('../../models/sellerModel');
+const { responseReturn } = require("../../utiles/response");
 const adminSellerMessage = require('../../models/chat/adminSellerMessage');
+const sellerCustomerMessage = require('../../models/chat/sellerCustomerMessage');
 
 class dashboardController {
     get_admin_dashboard_data = async(req, res) => {
@@ -33,7 +37,47 @@ class dashboardController {
         } catch (error) {
             console.log(error.message);
         }
-    } 
+    }
+
+    get_seller_dashboard_data = async (req, res) => {
+        const {id} = req;
+
+        try {
+            const totalSale = await sellerWallet.aggregate([
+                { $match: { sellerId: { $eq: id }} },
+                { $group: {_id:null, totalAmount: {$sum: '$amount'}} }
+            ]);
+
+            const totalOrder = await authOrder.find({ sellerId: new ObjectId(id) }).countDocuments();
+            const totalProduct = await productModel.find({ sellerId: new ObjectId(id) }).countDocuments();
+            const totalPendingOrder = await authOrder .find({
+                $and:[
+                    { sellerId: {$eq: new ObjectId(id)} },
+                    { delivery_status :{$eq: 'pending'} }
+                ]
+            }).countDocuments();
+
+            const messages = await sellerCustomerMessage.find({
+                $or: [
+                    { senderId: {$eq: id} },
+                    { receverId: {$eq: id} }
+                ]
+            }).limit(3);
+
+            const recentOrders = await authOrder.find({ sellerId: new ObjectId(id) }).limit(5);
+
+            responseReturn(res, 200, {
+                totalProduct,
+                totalOrder,
+                totalPendingOrder,
+                messages,
+                recentOrders,
+                totalSale: totalSale.length > 0 ? totalSale[0].totalAmount : 0,
+            });
+        } catch (error) {
+            console.log(error.message);
+        }
+    }
 }
 
 module.exports = new dashboardController();
