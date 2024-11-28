58350ebdeb0690903560cd1f357d0a48f9596f38
hemel <smhemel.eu@gmail.com>
2024-11-28 21:52:52 +0600

    Implement API to get banner - Backend

--- backend/routes/dashboard/dashboardRoutes.js
+++ backend/routes/dashboard/dashboardRoutes.js
@@ -4,5 +4,7 @@ const dashboardController = require('../../controllers/dasboard/dashboardControl
   
 router.get('/admin/get-dashboard-data', authMiddleware, dashboardController.get_admin_dashboard_data);
 router.get('/seller/get-dashboard-data', authMiddleware, dashboardController.get_seller_dashboard_data);
+
+router.get('/banner/get/:productId', authMiddleware, dashboardController.get_banner);
     
 module.exports = router;
