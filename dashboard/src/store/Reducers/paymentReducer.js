7f05bbc1bb7d5392a3a864f703ebb281d31f7bb2
hemel <smhemel.eu@gmail.com>
2024-11-28 15:20:55 +0600

    Implement reducer methods for get payment request and confirm payment request - Dashboard

--- dashboard/src/store/Reducers/paymentReducer.js
+++ dashboard/src/store/Reducers/paymentReducer.js
@@ -25,6 +25,30 @@ export const send_withdrowal_request = createAsyncThunk(
     }
 ) 
 
+export const get_payment_request = createAsyncThunk(
+    'payment/get_payment_request',
+    async(_, {rejectWithValue, fulfillWithValue}) => { 
+        try { 
+            const {data} = await api.get(`/payment/request`, {withCredentials: true});
+            return fulfillWithValue(data);
+        } catch (error) {
+            return rejectWithValue(error.response.data);
+        }
+    }
+)
+
+export const confirm_payment_request = createAsyncThunk(
+    'payment/confirm_payment_request',
+    async(paymentId , {rejectWithValue, fulfillWithValue}) => { 
+        try { 
+            const {data} = await api.post(`/payment/request-confirm`, {paymentId}, {withCredentials: true});
+            return fulfillWithValue(data);
+        } catch (error) {
+            return rejectWithValue(error.response.data);
+        }
+    }
+) 
+
 export const paymentReducer = createSlice({
     name: 'payment',
     initialState:{
@@ -52,7 +76,7 @@ export const paymentReducer = createSlice({
         state.totalAmount = payload.totalAmount;
         state.availableAmount = payload.availableAmount;
         state.withdrowAmount = payload.withdrowAmount;
-        state.pendingAmount = payload.availableAmount; 
+        state.pendingAmount = payload.pendingAmount; 
         })
         .addCase(send_withdrowal_request.pending, (state, { payload }) => {
             state.loader = true;
@@ -64,9 +88,26 @@ export const paymentReducer = createSlice({
         .addCase(send_withdrowal_request.fulfilled, (state, { payload }) => {
             state.loader = false;
             state.successMessage = payload.message; 
+            state.pendingAmount = payload.withdrowal.amount;
             state.pendingWithdrows = [...state.pendingWithdrows,payload.withdrowal]; 
             state.availableAmount = state.availableAmount - payload.withdrowal.amount; 
-            state.pendingAmount = payload.withdrowal.amount; 
+        })
+        .addCase(get_payment_request.fulfilled, (state, { payload }) => {
+            state.pendingWithdrows = payload.withdrowalRequest; 
+            
+        })
+        .addCase(confirm_payment_request.pending, (state, { payload }) => {
+            state.loader = true;
+        })
+        .addCase(confirm_payment_request.rejected, (state, { payload }) => {
+            state.loader = false;
+            state.errorMessage = payload.message; 
+        })
+        .addCase(confirm_payment_request.fulfilled, (state, { payload }) => {
+            const temp = state.pendingWithdrows.filter(r => r._id !== payload.payment._id);
+            state.loader = false;
+            state.successMessage = payload.message;
+            state.pendingWithdrows = temp;
         })
     }
 })
